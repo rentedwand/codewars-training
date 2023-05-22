2d751a551678f7f2d5d26ab37651055631ccc3e5
Maxim Chuev <maksimiliano007@gmail.com>
2023-05-23 00:02:57 +0300

    my first codewars task

---
+++ Grade book.js
@@ -0,0 +1,20 @@
+function getGrade (s1, s2, s3) {
+    // Code here
+    const sum = (s1 + s2 + s3) / 3;
+    if (sum >= 90 && sum <= 100) {
+      return "A";
+      }
+    else if (sum >=80 && sum < 90) {
+      return "B";
+    }
+    else if (sum >=70 && sum < 80) {
+      return "C";
+    }
+     else if (sum >=60 && sum < 70) {
+      return "D";
+    }
+     else if (sum >=0 && sum < 60) {
+      return "F";
+    }
+  }
+    
